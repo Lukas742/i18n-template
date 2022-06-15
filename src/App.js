621f5a1a073dc9bab8632382cb7fb3c964cd6501
Harbarth, Lukas <lukas.harbarth@sap.com>
2022-06-15 17:52:07 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
 import {
   FlexBox,
-  FlexBoxAlignItems,
   FlexBoxDirection,
-  FlexBoxJustifyContent,
-  Link,
-  LinkDesign,
-  ShellBar,
-  ThemeProvider
-} from '@ui5/webcomponents-react';
-import './App.css';
+  IllustratedMessage,
+  Label,
+  Option,
+  Select,
+  Title,
+} from "@ui5/webcomponents-react";
+import LanguageCodes from "./LanguageCodes.json";
+import { setLanguage } from "@ui5/webcomponents-base/dist/config/Language.js";
+import { useEffect, useState } from "react";
+import { useI18nBundle } from "@ui5/webcomponents-react-base";
 
 function App() {
+  const i18nBundle = useI18nBundle("i18n-template");
+  const [language, setStateLanguage] = useState("en");
+
+  const handleLanguageChange = (e) => {
+    setStateLanguage(e.detail.selectedOption.dataset.codeId);
+  };
+
+  useEffect(() => {
+    setLanguage(language);
+  }, [language]);
+
   return (
-    <ThemeProvider>
-      <ShellBar primaryTitle="UI5 Web Components for React Template" />
-      <FlexBox
-        style={{ width: '100%', height: '100vh' }}
-        direction={FlexBoxDirection.Column}
-        justifyContent={FlexBoxJustifyContent.Center}
-        alignItems={FlexBoxAlignItems.Center}
-      >
-        <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
-          Getting Started with UI5 Web Component for React
-        </Link>
+    <>
+      <Label>Change language:</Label>
+      <br />
+      <Select onChange={handleLanguageChange}>
+        {LanguageCodes.map((item) => (
+          <Option
+            data-code-id={item.id}
+            key={item.id}
+            selected={item.id === language}
+          >
+            {item.language}
+          </Option>
+        ))}
+      </Select>
+      <br />
+      <br />
+      <FlexBox direction={FlexBoxDirection.Column}>
+        <Label>
+          This custom translation ("Please wait") will be translated for
+          English, German, French and Spanish for all other, the default
+          language (en) will be used.
+        </Label>
+        <Title>{i18nBundle.getText("PLEASE_WAIT")}</Title>
       </FlexBox>
-    </ThemeProvider>
+      <br />
+      <br />
+      <Label>
+        The `IllustratedMessage` component has static texts that are
+        translatable.
+      </Label>
+      <IllustratedMessage />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "@ui5/webcomponents-react/dist/Assets";
 
-const container = document.getElementById('root');
+// register loader function
+import { registerI18nLoader } from "@ui5/webcomponents-base/dist/asset-registries/i18n.js";
+import parse from "@ui5/webcomponents-base/dist/PropertiesFileFormat.js";
+import { ThemeProvider } from "@ui5/webcomponents-react";
+
+// note: for the default language (en) to be fetched from the "server" fetchDefaultLanguage has to be set to `true` (see index.html), otherwise custom translations for english will be missing
+["en", "fr", "de", "es"].forEach((localeToRegister) => {
+  registerI18nLoader("i18n-template", localeToRegister, async (localeId) => {
+    const props = await (
+      await fetch(`./locales/messagebundle_${localeId}.properties`)
+    ).text();
+    return parse(props);
+  });
+});
+
+const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+);
